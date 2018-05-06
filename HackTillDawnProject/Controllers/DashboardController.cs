1229cd2184c2dbfbd3f64baffdf7b5deefe8603a
Nick Spencer <nicholas.j.spencer1@gmail.com>
2018-05-05 18:48:35 -0700

    hi

---
+++ HackTillDawnProject/Controllers/DashboardController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HackTillDawnProject.Models;
+using HackTillDawnProject.Models.JsonModels;
+
+
+namespace HackTillDawnProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        /* Services */
+        IFootageStorageService _FootageStorageService;
+        public DashboardController(IFootageStorageService ifss)
+        {
+            _FootageStorageService = ifss;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public GetAllCurrentNotifications()
+        {
+            List<NotificationJson> notifications = new List<NotificationJson>();
+
+            List<FootageStorage> videos = _FootageStorageService.GetAllCurrent();
+            
+
+        }
+    }
+}
