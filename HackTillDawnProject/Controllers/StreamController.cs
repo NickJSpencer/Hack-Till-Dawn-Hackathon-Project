3af1cc3d497ecc9db0a431f22c1c6011fc616aff
Mitchell Marsh <mmarsh@scale7.co>
2018-05-05 19:35:27 -0700

    Streaming

---
+++ HackTillDawnProject/Controllers/StreamController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HackTillDawnProject.Controllers
+{
+    public class StreamController : Controller
+    {
+        public async Task Index()
+        {
+            var response = HttpContext.Response;
+            response.Headers.Add("Content-Type", "video/abst");
+
+            for (var i = 0; true; ++i)
+            {
+                await response
+                    .WriteAsync($"data: Controller {i} at {DateTime.Now}\r\r");
+
+                response.Body.Flush();
+                await Task.Delay(5 * 1000);
+            }
+        }
+    }
+}
---
+++ HackTillDawnProject/ModelManager/EventManager.cs
@@ -0,0 +1,19 @@
+﻿using HackTillDawnProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackTillDawnProject.ModelManager
+{
+    public class EventManager
+    {
+        public Event CreateNewEvent(string EventName)
+        {
+            return new Event()
+            {
+                Name = EventName
+            };
+        }
+    }
+}
